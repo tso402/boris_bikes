a3b42cf3c67a01d21b81931d1242334fff74ecbd
Student <student@Admins-MacBook-Pro.local>
2019-02-05 17:26:51 +0000

    Closing for the day on step 9

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,5 +2,6 @@
 require "./lib/docking_station"
 describe DockingStation do
   #it 'release_bike' do
+  #it {expect(DockingStation).to respond_to (:release_bike)}
   it {is_expected.to respond_to :release_bike}
 end
