fa5d8e601d7cc01a18fd607666fcb847abef51b1
Maria Vanaale <mvanaale@gmail.com>
2019-02-05 16:19:50 +0000

    Step 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,7 @@
 # In spec/borisbike_spec.rb
 require "./lib/docking_station"
 describe DockingStation do
+  it 'release_bike' do
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
