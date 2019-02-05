2fc155967be50ad970231e3bfb97530dde0fe319
Student <student@Admins-MacBook-Pro.local>
2019-02-05 16:44:47 +0000

    Step 8 release_bike method created

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,6 @@
 # In spec/borisbike_spec.rb
 require "./lib/docking_station"
 describe DockingStation do
-  it 'release_bike' do
-    expect(docking_station).to respond_to(:release_bike)
-  end
+  #it 'release_bike' do
+  it {is_expected.to respond_to :release_bike}
 end
