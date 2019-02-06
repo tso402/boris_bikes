47520e1442ee69b65fd12b34d9c0a7edc3aac882
James Ault <james.ault@johnlewis.co.uk>
2019-02-06 17:54:54 +0000

    Step 15 Single Responsibility Refactor

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,16 +7,22 @@ class DockingStation
   end
 
   def release_bike
-      if @stored_bikes.length == 0
-        fail 'Error, no bikes available!'
-      else
-      return bike = Bike.new
-    end
+      fail 'Error, no bikes available!' if empty?
+      @stored_bikes.pop
   end
 
   def dock_bike (bike)
-    fail 'Error, Another bike already docked' unless @stored_bikes.length < 20
+    fail 'Error, Another bike already docked' if full?
     @stored_bikes << bike
-    return @stored_bikes
+    #return @stored_bikes
+  end
+
+private
+  def full?
+    @stored_bikes.count >= 20
+  end
+
+  def empty?
+    @stored_bikes.empty?
   end
 end
